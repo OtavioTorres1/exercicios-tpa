eed3530a752fce1fe15cab700330e73ab91bf7d6
OtavioTorres1 <torresotavio702@gmail.com>
2024-09-24 17:09:30 -0300

    Add files via upload

---
+++ Lista01/HelloWorld.java
@@ -0,0 +1,8 @@
+package lista01;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		//escreva ("Alo Mundo")
+		System.out.println("Hello World");
+		System.out.println("Olá Mundo");
+	}}
---
+++ Lista01/Medias.java
@@ -0,0 +1,13 @@
+package lista01;
+
+public class Medias {
+	public static void main(String args[]) {
+		//declaração de variáveis
+		double n1, n2, m;
+		
+		n1 = 8.5;
+		n2 = 6;
+		m = (n1+n2)/2;
+		System.out.println("Sua média é "+ m + " pontos ");
+	}
+}
